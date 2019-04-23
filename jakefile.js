2039b6225bdf21cf6febcfb27ad11be9ad112c13
hemapriya <hemapriyasampath@gmail.com>
2019-04-23 13:14:11 -0400

    version checking and add semver (node parser)

--- jakefile.js
+++ jakefile.js
@@ -1,14 +1,26 @@
 (function(){
 "use strict";
 
+var semver = require("semver");
+
 desc(" task description");
     task("default",["version"],function(){
         console.log("Build OK ");
     });
 
-    desc("version");
+desc("version");
     task("version",function(){
-        console.log("checking node version:.")
+        console.log("checking node version:.");
+
+        var packageJson = require("./package.json");
+        var expectedVersion = "v"+packageJson.engines.node;
+        var actualVersion = process.version;
+       
+          if(semver.neq(expectedVersion,actualVersion)){
+              fail("Incorrect node version : Expected"+expectedVersion +"but was actual"+actualVersion);
+          }
+
     });
+
 
 }());
