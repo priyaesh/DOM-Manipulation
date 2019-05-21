593df81ea2b5fd6b4604147457492254b3841629
hemapriya <hemapriyasampath@gmail.com>
2019-05-21 13:57:36 -0400

    chai-2.1

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/**/*.js'
+      'src/**/*.js',
+      'src/vendor/**/*.js'
     ],
 
 
@@ -27,7 +28,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/**/*.js':'commonjs'
+      'src/**/*.js':'commonjs',
+      'src/vendor/**/*.js':'commonjs'
     },
 
 
