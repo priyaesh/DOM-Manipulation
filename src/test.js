900c8f78568a8f9b4f23fceeb074a772f27e8e59
hemapriya <hemapriyasampath@gmail.com>
2019-05-21 14:02:16 -0400

    assert.equal in chai

--- src/test.js
+++ src/test.js
@@ -2,28 +2,21 @@
     "use strict";
 
     var addition = require("./add.js");
+    var assert = require("./vendor/chai-2.1.js").assert;
 
-   // var assert = require("chai").assert;
-
-    // var actual = add(3,4);
-    // var expected =7;
-
-    // assert.equal(add(3,4),7);
-
-    // if(actual !== expected) throw new Error("Expected "+expected ,"actual was"+actual);
-
+    
     describe("Adding two numbers",function(){
          it("addition",function(){
-            assertEqual(addition.add(3,4), 7);
+            assert.equal(addition.add(3,4), 7);
          });
          it("adding floating point numbers",function(){
-            assertEqual(addition.add(3.2,0.0), 3.2);
+            assert.equal(addition.add(3.2,0.0), 3.2);
          });
 
     });
-    function assertEqual(actual, expected){
-        if(actual !== expected) throw new Error("Expected "+expected ,"actual was"+actual);
-    }
+    // function assertEqual(actual, expected){
+    //     if(actual !== expected) throw new Error("Expected "+expected ,"actual was"+actual);
+    // }
 
     
     
