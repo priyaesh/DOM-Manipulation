a71db4d02ae74cac307afb14ad429ffe389ce617
hemapriya <hemapriyasampath@gmail.com>
2019-05-22 13:23:20 -0400

    bare bone test

--- src/test.js
+++ src/test.js
@@ -1,23 +1,14 @@
 (function(){
     "use strict";
 
-    var addition = require("./add.js");
-    var assert = require("./assert.js");
+   var assert = require("./assert.js");
 
     
-    describe("Adding two numbers",function(){
-         it("addition",function(){
-            assert.equal(addition.add(3,4), 7);
-         });
-         it("adding floating point numbers",function(){
-            assert.equal(addition.add(3.2,0.0), 3.2);
-         });
-
+    describe("something",function(){
+         it("something",function(){
+           
+        });
+      
     });
-    // function assertEqual(actual, expected){
-    //     if(actual !== expected) throw new Error("Expected "+expected ,"actual was"+actual);
-    // }
-
-    
-    
+   
 }());
