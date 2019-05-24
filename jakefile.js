a58fb2ee7b080bf8601fe949defb716cda711050
hemapriya <hemapriyasampath@gmail.com>
2019-05-24 13:25:04 -0400

    add css to html file

--- jakefile.js
+++ jakefile.js
@@ -33,7 +33,7 @@
             console.log("Building distribution directory:.");
     
              shell.rm("-rf",DIST_DIR +"/*");
-             shell.cp("src/index.html",DIST_DIR);
+             shell.cp("src//content/*",DIST_DIR);
     
              jake.exec("node node_modules/browserify/bin/cmd.js  src/app.js -o " + DIST_DIR + "/bundle.js",
              {interactive:true},
--- src/test.js
+++ src/test.js
@@ -2,13 +2,29 @@
     "use strict";
 
    var assert = require("./assert.js");
-
     
-    describe("something",function(){
+   describe("something",function(){
+       
          it("something",function(){
+
+            var div= document.createElement("div");
+
+            div.innerHTML = "This is an example";
+
+            document.body.appendChild(div);
+
+            var p = document.createElement("p");
+            p.innerHTML = "This is embedded paragraph";
+            div.appendChild(p);
+
+            div.parentNode.removeChild(div);
+
+
            
         });
       
     });
+
+   
    
 }());
